004d3b9a4f289949b463560267fcb231af706351
andromediano <andromediano@gmail.com>
2021-09-12 12:33:26 +0900

    Obsidian 링크가 레이블을 가지지 않은 경우 링크를 레이블로 사용하게 한다

--- static/custom.js
+++ static/custom.js
@@ -1,22 +1,23 @@
-function changeObsidianLink(element) {
+function convertObsidianLink(element) {
   // Node.ELEMENT_NODE: 1
   // Node.ATTRIBUTE_NODE: 2
   // Node.TEXT_NODE: 3
   if (element.childNodes.length) {
     // Node.ELEMENT_NODE 인 경우만 들어 오겠지...
     //console.log(element.nodeType);
-    element.childNodes.forEach(node => changeObsidianLink(node));
+    element.childNodes.forEach(node => convertObsidianLink(node));
   } else {
       const text = element.textContent;
       if (text) {
         let regexp = new RegExp('(.*)\\[\\[(.*)\\]\\](.*)', 'g');
         if (regexp.test(text)) {
-          element.parentNode.innerHTML = '<a href="' + RegExp.$2 + '">' + RegExp.$1 + '</a>' + RegExp.$3;
+          element.parentNode.innerHTML = `<a href="${RegExp.$2}">${(RegExp.$1.length === 0) ? RegExp.$2 : RegExp.$1}</a>`;
+          //console.log(`<a href="${RegExp.$2}">${(RegExp.$1.length === 0) ? RegExp.$2 : RegExp.$1}</a>`);
         }
       }
   }
 };
 
 (function() {
-  changeObsidianLink(document.querySelector("article.article"));
+  convertObsidianLink(document.querySelector("article.article"));
 })()
